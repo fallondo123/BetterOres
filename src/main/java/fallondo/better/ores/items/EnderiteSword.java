436dd212bbfe8398f6f2df303f3e7a0dffbf41be
fallondo123 <70604407+fallondo123@users.noreply.github.com>
2021-03-20 17:14:53 +0200

    0.0.8 snapshot

---
+++ src/main/java/fallondo/better/ores/items/EnderiteSword.java
@@ -0,0 +1,38 @@
+package fallondo.better.ores.items;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class EnderiteSword extends SwordItem {
+
+    public EnderiteSword(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_ENDER_PEARL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (RANDOM.nextFloat() * 0.4F + 0.8F));
+        user.getItemCooldownManager().set(this, 20);
+        if (!world.isClient) {
+            EnderPearlEntity enderPearlEntity = new EnderPearlEntity(world, user);
+            enderPearlEntity.setItem(ItemStack.EMPTY);
+            enderPearlEntity.setProperties(user, user.pitch, user.yaw, 0.0F, 1.5F, 1.0F);
+            world.spawnEntity(enderPearlEntity);
+        }
+
+        //user.incrementStat(Stats.USED.getOrCreateStat(this));
+        //if (!user.abilities.creativeMode) {
+        //    itemStack.decrement(1);
+        //}
+
+        return TypedActionResult.success(itemStack, world.isClient());
+    }
+}
